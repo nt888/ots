6a2327541f1c95f7e469e61c38f5425bc8757e8b
nt <neotune888@gmail.com>
2022-09-10 21:37:56 +0300

    done

--- generate_data.lua
+++ generate_data.lua
@@ -1,14 +1,14 @@
 #!/bin/lua5.4
 
+-- local Vmod = 600
+-- local Vinf = 1200
+-- local q = 4
+-- local m = 2
 local f0 = 2400 -- Hz
-local Vmod = 600
-local Vinf = 1200
 local T = 1 / 600
 local A = 1 -- TODO: why?
-local Ns = 8
+local Ns = 32
 local dt = (1 / f0) / Ns
-local q = 4
-local m = 2
 
 local t = {}
 for i = 0, T, dt do
@@ -19,16 +19,21 @@ local Si1 = { A, A, -A, -A }
 local Si2 = { A, -A, A, -A }
 
 local S = {}
-for _, tval in ipairs(t) do
-    S[#S + 1] = Si1[1] * math.sqrt(2 / T) * math.cos(2 * math.pi * f0 * tval)
-        + Si2[1] * math.sqrt(2 / T) * math.sin(2 * math.pi * f0 * tval)
+for i = 1, #t do
+    S[i] = {}
+    for j = 1, #Si1 do
+        S[i][j] = Si1[j] * math.sqrt(2 / T) * math.cos(2 * math.pi * f0 * t[i])
+            + Si2[j] * math.sqrt(2 / T) * math.sin(2 * math.pi * f0 * t[i])
+    end
 end
-
 
 local outfile = assert(io.open("data.csv", "w"))
 
-for i = 1, #t do
-    outfile:write(t[i] .. "," .. S[i], "\n")
+for i = 1, #S do
+    outfile:write(t[i], ",")
+    for j = 1, #S[i] do
+        outfile:write(S[i][j], ",") end
+    outfile:write("\n")
 end
 
 outfile:close()
